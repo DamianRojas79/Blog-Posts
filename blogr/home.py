15405495e6e8b0006cd245be917ca0797101d73a
Damian Rojas <rojasdamianc@gmail.com>
2024-02-29 22:47:42 -0300

    Agregando vistas y uniendo a flask

--- blogr/home.py
+++ blogr/home.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp=Blueprint ('home',__name__)
 
 @bp.route('/')
 def index():
-    return 'Pagina de Inicio'
+    return render_template ('index.html')
 
 
 @bp.route('/blog')
 def blog():
-    return 'Pagina de Blog'
+    return render_template('blog.html')
